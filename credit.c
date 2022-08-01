462bf15a580f67702c0506e0905a16cb8b479411
shadeprojects <110209863+shadeprojects@users.noreply.github.com>
2022-08-01 16:29:02 -0700

    Update credit.c

--- credit.c
+++ credit.c
@@ -6,86 +6,214 @@ int main(void)
     int count = 0;
     long card_number = get_long("Enter credit card number: ");
     long card_counter = card_number;
-    while(card_counter > 0)
+    while (card_counter > 0)
     {
         card_counter /= 10;
         count++;
     }
     int last_number = card_number % 10;
-    int second_to_last_number =  (card_number / 10) % 10;
-    int third_to_last_number = ((card_number / 10)/ 10) % 10;
-    int fourth_to_last_number = (((card_number / 10)/ 10)/ 10) % 10;
-    int fifth_to_last_number = ((((card_number / 10)/ 10)/ 10)/ 10) % 10;
-    int sixth_to_last_number = (((((card_number / 10)/ 10)/ 10)/ 10)/ 10) % 10;
-    int seventh_to_last_number = ((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-    int eighth_to_last_number = (((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-    int ninth_to_last_number = ((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-    int tenth_to_last_number = (((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-    int eleventh_to_last_number = ((((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-    int twelfth_to_last_number = (((((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-    int thirteenth_to_last_number = ((((((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-    int fourteenth_to_last_number = (((((((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-    int fifteenth_to_last_number = ((((((((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-    int sixteenth_to_last_number = (((((((((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
+    int second_to_last_number = (card_number / 10) % 10;
+    int third_to_last_number = ((card_number / 10) / 10) % 10;
+    int fourth_to_last_number = (((card_number / 10) / 10) / 10) % 10;
+    int fifth_to_last_number = ((((card_number / 10) / 10) / 10) / 10) % 10;
+    int sixth_to_last_number = (((((card_number / 10) / 10) / 10) / 10) / 10) % 10;
+    int seventh_to_last_number = ((((((card_number / 10) / 10) / 10) / 10) / 10) / 10) % 10;
+    int eighth_to_last_number = (((((((card_number / 10) / 10) / 10) / 10) / 10) / 10) / 10) % 10;
+    int ninth_to_last_number = ((((((((card_number / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) % 10;
+    int tenth_to_last_number = (((((((((card_number / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) % 10;
+    int eleventh_to_last_number = ((((((((((card_number / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) % 10;
+    int twelfth_to_last_number = (((((((((((card_number / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) % 10;
+    int thirteenth_to_last_number = ((((((((((((card_number / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) % 10;
+    int fourteenth_to_last_number = (((((((((((((card_number / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) % 10;
+    int fifteenth_to_last_number = ((((((((((((((card_number / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) % 10;
+    int sixteenth_to_last_number = (((((((((((((((card_number / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) / 10) % 10;
     if (count == 16)
     {
-        if ((fifteenth_to_last_number == 1 || fifteenth_to_last_number == 2 || fifteenth_to_last_number == 3 || fifteenth_to_last_number == 4 || fifteenth_to_last_number == 5) && sixteenth_to_last_number == 5)
+        int first_number = sixteenth_to_last_number;
+        int second_to_last_multiplication = second_to_last_number * 2;
+        int fourth_to_last_multiplication = fourth_to_last_number * 2;
+        int sixth_to_last_multiplication = sixth_to_last_number * 2;
+        int eighth_to_last_multiplication = eighth_to_last_number * 2;
+        int tenth_to_last_multiplication = tenth_to_last_number * 2;
+        int twelfth_to_last_multiplication = twelfth_to_last_number * 2;
+        int fourteenth_to_last_multiplication = fourteenth_to_last_number * 2;
+        int first_number_multiplication = first_number * 2;
+        if (second_to_last_multiplication >= 10)
         {
-            printf("Your card is a Mastercard card. The last number is %i. The second to last number is %i. Fourth to last is %i. Sixth to last is %i. Eighth to last is %i. Tenth to last is %i. Twelfth to last is %i. Fourteenth to last is %i. Sixteenth to last is %i.\n", last_number, second_to_last_number, fourth_to_last_number, sixth_to_last_number, eighth_to_last_number, tenth_to_last_number, twelfth_to_last_number, fourteenth_to_last_number, sixteenth_to_last_number);
-            return card_number;
+            second_to_last_multiplication = (second_to_last_multiplication - 10) + 1;
         }
-        if (sixteenth_to_last_number == 4)
+        if (fourth_to_last_multiplication >= 10)
         {
-            printf("Your card is a Visa card.\n");
+            fourth_to_last_multiplication = (fourth_to_last_multiplication - 10) + 1;
+        }
+        if (sixth_to_last_multiplication >= 10)
+        {
+            sixth_to_last_multiplication = (sixth_to_last_multiplication - 10) + 1;
+        }
+        if (eighth_to_last_multiplication >= 10)
+        {
+            eighth_to_last_multiplication = (eighth_to_last_multiplication - 10) + 1;
+        }
+        if (tenth_to_last_multiplication >= 10)
+        {
+            tenth_to_last_multiplication = (tenth_to_last_multiplication - 10) + 1;
+        }
+        if (twelfth_to_last_multiplication >= 10)
+        {
+            twelfth_to_last_multiplication = (twelfth_to_last_multiplication - 10) + 1;
+        }
+        if (fourteenth_to_last_multiplication >= 10)
+        {
+            fourteenth_to_last_multiplication = (fourteenth_to_last_multiplication - 10) + 1;
+        }
+        if (first_number_multiplication >= 10)
+        {
+            first_number_multiplication = (first_number_multiplication - 10) + 1;
+        }
+        int checksum_addition = last_number + second_to_last_multiplication + third_to_last_number + fourth_to_last_multiplication +
+                                fifth_to_last_number + sixth_to_last_multiplication + seventh_to_last_number + eighth_to_last_multiplication + ninth_to_last_number
+                                + tenth_to_last_multiplication + eleventh_to_last_number + twelfth_to_last_multiplication + thirteenth_to_last_number +
+                                fourteenth_to_last_multiplication + fifteenth_to_last_number + first_number_multiplication;
+        if ((fifteenth_to_last_number == 1 || fifteenth_to_last_number == 2 || fifteenth_to_last_number == 3
+             || fifteenth_to_last_number == 4 || fifteenth_to_last_number == 5) && first_number == 5)
+        {
+            if (checksum_addition % 10 == 0)
+            {
+                printf("MASTERCARD\n");
+            }
+            else
+            {
+                printf("INVALID\n");
+            }
+        }
+        else if (sixteenth_to_last_number == 4)
+        {
+            if (checksum_addition % 10 == 0)
+            {
+                printf("VISA\n");
+            }
+            else
+            {
+                printf("INVALID\n");
+            }
+        }
+        else
+        {
+            printf("INVALID\n");
         }
     }
-    if (count == 15)
+    else if (count == 15)
     {
-        if ((fourteenth_to_last_number == 4 || fourteenth_to_last_number == 7) && fifteenth_to_last_number == 3)
+        int first_number = fifteenth_to_last_number;
+        int second_to_last_multiplication = second_to_last_number * 2;
+        int fourth_to_last_multiplication = fourth_to_last_number * 2;
+        int sixth_to_last_multiplication = sixth_to_last_number * 2;
+        int eighth_to_last_multiplication = eighth_to_last_number * 2;
+        int tenth_to_last_multiplication = tenth_to_last_number * 2;
+        int twelfth_to_last_multiplication = twelfth_to_last_number * 2;
+        int fourteenth_to_last_multiplication = fourteenth_to_last_number * 2;
+        if (second_to_last_multiplication >= 10)
         {
-            printf("Your card is an American Express card.\n");
+            second_to_last_multiplication = (second_to_last_multiplication - 10) + 1;
+        }
+        if (fourth_to_last_multiplication >= 10)
+        {
+            fourth_to_last_multiplication = (fourth_to_last_multiplication - 10) + 1;
+        }
+        if (sixth_to_last_multiplication >= 10)
+        {
+            sixth_to_last_multiplication = (sixth_to_last_multiplication - 10) + 1;
+        }
+        if (eighth_to_last_multiplication >= 10)
+        {
+            eighth_to_last_multiplication = (eighth_to_last_multiplication - 10) + 1;
+        }
+        if (tenth_to_last_multiplication >= 10)
+        {
+            tenth_to_last_multiplication = (tenth_to_last_multiplication - 10) + 1;
+        }
+        if (twelfth_to_last_multiplication >= 10)
+        {
+            twelfth_to_last_multiplication = (twelfth_to_last_multiplication - 10) + 1;
+        }
+        if (fourteenth_to_last_multiplication >= 10)
+        {
+            fourteenth_to_last_multiplication = (fourteenth_to_last_multiplication - 10) + 1;
+        }
+        int checksum_addition = last_number + second_to_last_multiplication + third_to_last_number + fourth_to_last_multiplication +
+                                fifth_to_last_number + sixth_to_last_multiplication + seventh_to_last_number + eighth_to_last_multiplication + ninth_to_last_number
+                                + tenth_to_last_multiplication + eleventh_to_last_number + twelfth_to_last_multiplication + thirteenth_to_last_number +
+                                fourteenth_to_last_multiplication + first_number;
+        if ((fourteenth_to_last_number == 4 || fourteenth_to_last_number == 7) && first_number == 3)
+        {
+            if (checksum_addition % 10 == 0)
+            {
+                printf("AMEX\n");
+            }
+            else
+            {
+                printf("INVALID\n");
+            }
+        }
+        else
+        {
+            printf("INVALID\n");
         }
     }
-    if (count == 13)
+    else if (count == 13)
     {
-        if (thirteenth_to_last_number == 4)
+        int first_number = thirteenth_to_last_number;
+        int second_to_last_multiplication = second_to_last_number * 2;
+        int fourth_to_last_multiplication = fourth_to_last_number * 2;
+        int sixth_to_last_multiplication = sixth_to_last_number * 2;
+        int eighth_to_last_multiplication = eighth_to_last_number * 2;
+        int tenth_to_last_multiplication = tenth_to_last_number * 2;
+        int twelfth_to_last_multiplication = twelfth_to_last_number * 2;
+        if (second_to_last_multiplication >= 10)
         {
-            printf("Your card is a Visa card.\n");
+            second_to_last_multiplication = (second_to_last_multiplication - 10) + 1;
+        }
+        if (fourth_to_last_multiplication >= 10)
+        {
+            fourth_to_last_multiplication = (fourth_to_last_multiplication - 10) + 1;
+        }
+        if (sixth_to_last_multiplication >= 10)
+        {
+            sixth_to_last_multiplication = (sixth_to_last_multiplication - 10) + 1;
+        }
+        if (eighth_to_last_multiplication >= 10)
+        {
+            eighth_to_last_multiplication = (eighth_to_last_multiplication - 10) + 1;
+        }
+        if (tenth_to_last_multiplication >= 10)
+        {
+            tenth_to_last_multiplication = (tenth_to_last_multiplication - 10) + 1;
+        }
+        if (twelfth_to_last_multiplication >= 10)
+        {
+            twelfth_to_last_multiplication = (twelfth_to_last_multiplication - 10) + 1;
+        }
+        int checksum_addition = last_number + second_to_last_multiplication + third_to_last_number + fourth_to_last_multiplication +
+                                fifth_to_last_number + sixth_to_last_multiplication + seventh_to_last_number + eighth_to_last_multiplication + ninth_to_last_number
+                                + tenth_to_last_multiplication + eleventh_to_last_number + twelfth_to_last_multiplication + first_number;
+        if (first_number == 4)
+        {
+            if (checksum_addition % 10 == 0)
+            {
+                printf("VISA\n");
+            }
+            else
+            {
+                printf("INVALID\n");
+            }
+        }
+        else
+        {
+            printf("INVALID\n");
         }
     }
     else
     {
-        printf("Invalid card number.\n");
+        printf("INVALID\n");
     }
-
-
-
-        //int last_number = card_number % 10;
-        //int second_to_last_number =  (card_number / 10) % 10;
-        //int third_to_last_number = ((card_number / 10)/ 10) % 10;
-        //int fourth_to_last_number = (((card_number / 10)/ 10)/ 10) % 10;
-        //int fifth_to_last_number = ((((card_number / 10)/ 10)/ 10)/ 10) % 10;
-        //int sixth_to_last_number = (((((card_number / 10)/ 10)/ 10)/ 10)/ 10) % 10;
-        //int seventh_to_last_number = ((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-        //int eighth_to_last_number = (((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-        //int ninth_to_last_number = ((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-        //int tenth_to_last_number = (((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-        //int eleventh_to_last_number = ((((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-        //int twelfth_to_last_number = (((((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-        //int thirteenth_to_last_number = ((((((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-        //int fourteenth_to_last_number = (((((((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-        //int fifteenth_to_last_number = ((((((((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-        //int sixteenth_to_last_number = (((((((((((((((card_number / 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10)/ 10) % 10;
-        //int checksum_multiplication =
-
-
-
-
-
-
-
-
-
-
-    //printf("Your credit card number is: %li, the last number is %i, the second to last number is %i, the third to last number is %i\n", card_number, last_number, second_to_last_number, third_to_last_number);
 }
